264f3e83266eb46bb18a2767c36f6fba6c3250f7
awab mahmod <awabmahmod408@gmail.com>
2026-03-15 23:03:16 +0200

    interactive json master plan

---
+++ Brochure-3/facilities-coords.js
@@ -0,0 +1,81 @@
+var facilitiesCoordsData = [
+  {
+    "n_instances": 1,
+    "n_x_coordinate": [
+      61.048946093632416
+    ],
+    "n_y_coordinate": [
+      11.87971074217712
+    ]
+  },
+  {
+    "n_instances": 1,
+    "n_x_coordinate": 0,
+    "n_y_coordinate": 0
+  },
+  {
+    "n_instances": 1,
+    "n_x_coordinate": 0,
+    "n_y_coordinate": 0
+  },
+  {
+    "n_instances": 1,
+    "n_x_coordinate": 0,
+    "n_y_coordinate": 0
+  },
+  {
+    "n_instances": 1,
+    "n_x_coordinate": 0,
+    "n_y_coordinate": 0
+  },
+  {
+    "n_instances": 1,
+    "n_x_coordinate": 0,
+    "n_y_coordinate": 0
+  },
+  {
+    "n_instances": 1,
+    "n_x_coordinate": 0,
+    "n_y_coordinate": 0
+  },
+  {
+    "n_instances": 1,
+    "n_x_coordinate": 0,
+    "n_y_coordinate": 0
+  },
+  {
+    "n_instances": 1,
+    "n_x_coordinate": 0,
+    "n_y_coordinate": 0
+  },
+  {
+    "n_instances": 1,
+    "n_x_coordinate": 0,
+    "n_y_coordinate": 0
+  },
+  {
+    "n_instances": 1,
+    "n_x_coordinate": 0,
+    "n_y_coordinate": 0
+  },
+  {
+    "n_instances": 1,
+    "n_x_coordinate": 0,
+    "n_y_coordinate": 0
+  },
+  {
+    "n_instances": 1,
+    "n_x_coordinate": 0,
+    "n_y_coordinate": 0
+  },
+  {
+    "n_instances": 1,
+    "n_x_coordinate": 0,
+    "n_y_coordinate": 0
+  },
+  {
+    "n_instances": 1,
+    "n_x_coordinate": 0,
+    "n_y_coordinate": 0
+  }
+];
